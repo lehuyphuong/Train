c6078fd53b8c1f8c09ad5fe9fc3511c276e2ea18
le huy phuong <huyphuongvt27@gmail.com>
2025-04-12 20:00:14 +0700

    modify path

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ if __name__ == "__main__":
     parser.add_argument("-ckp", default=checkpoint, type=Path,		help="Path to make checkpoint during training ..... default: 'checkpoint.tar'")
 
     util.seed_everything(42)
-    folder_path = "/mimer/NOBACKUP/groups/alvis_cvl/cuole/phys_grd/dataset_" + args.test_subject
+    folder_path = "/content/ProcessedData/ProcessedData" + args.test_subject
     # Trainer(**vars(parser.parse_args()))
     Trainer(**vars(parser.parse_args())).run()
 
